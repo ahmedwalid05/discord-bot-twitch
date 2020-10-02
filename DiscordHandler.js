4088ca5156e2720b0a8843da2c635c1cff4b2c31
Ahmad Massoud <ahmedwalid05@gmail.com>
2020-10-02 23:05:59 +0300

    - Added proper Logging - Added dynamic cooldown peroid - Fixed bugs

--- DiscordHandler.js
+++ DiscordHandler.js
@@ -1,43 +1,88 @@
 const Discord = require('discord.js');
-
+const { format, loggers, transports } = require('winston')
+const logger = loggers.get('default')
 
 class DiscordHandler {
 
   constructor(config, callback) {
+
     this.config = config;
     this.client = new Discord.Client();
     let client = this.client;
+    this.allowedToWork = 1;
 
     client.on('ready', () => {
-      console.log(`Logged in as ${client.user.tag}!`);
+      logger.info(`Logged in as ${client.user.tag}!`);
       if (callback)
         callback();
     });
     // client.on('raw', console.log);
     client.on('shardError', error => {
-      console.error('A websocket connection encountered an error:', error);
+      logger.error('A websocket connection encountered an error:', error);
     });
     client.on('rateLimit', (rateLimitInfo) => {
-      console.log(`Discord Has Rate limited you, please try again after ${rateLimitInfo.timeout / 1000}`)
+      logger.warn(`Discord Has Rate limited you, please try again after ${rateLimitInfo.timeout / 1000}`)
     })
     // client.on('debug', (info) => {
     //   console.log(info);
     // })
     client.on('error', (error) => {
-      console.log(error)
-    }
-    )
+      logger.error(`Error Occurred: ${error}`)
+    })
 
+    client.on('warn', (error) => {
+      logger.warn(`Error Occurred: ${error}`)
+    })
     client.login(this.config.discord_bot_token);
   }
 
-  async changeName(online) {
+  async changeName(name) {
+    logger.debug
+    this.allowedToWork--;
+    if (this.allowedToWork > 0) {
+      return Promise.reject({
+        canceled: true,
+        reason: "A new status was found"
+      })
+    }
     let channel = await this.client.channels.resolve(this.config.channel_Id).fetch(true);
-    let name = (online ? this.config.online_text : this.config.offline_text);
-    console.log(`Stream Changed Status: ${name}`);
-    return channel.edit({ name: name})
+    // let name = (online ? this.config.online_text : this.config.offline_text);
+
+    if (name.toLowerCase().localeCompare(channel.name) == 0) {
+      return Promise.reject({
+        canceled: true,
+        reason: "Channel Name is the same as the new channel name"
+      })
+    }
+    this.lastChannelUpdateTime = Date.now();
+    let promise = channel.edit({ name: name })
+    logger.debug(`Changing Name to ${name}`)
+    return promise;
+    // return Promise.resolve();
   }
 
+  async waitForTime(name) {
+    logger.debug(`Scheduling a new channel change event`)
+    // this.valueToExecute = name;
+    if (this.lastChannelUpdateTime) {
+      let timeDiff = Date.now() - this.lastChannelUpdateTime;
+      let cooldown = this.config.cooldown_time * 1000 - timeDiff;
+      this.allowedToWork++;
+      if (cooldown > 0) {
+        logger.info(`Sleeping For ${cooldown / 1000} seconds`)
+        await this.sleep(cooldown);
+      }
+
+    }
+    return this.changeName(name);
+
+    // return promise
+  }
+  async sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
 
 }
 module.exports = DiscordHandler
--- index.js
+++ index.js
@@ -1,21 +1,46 @@
+//Twitch Library
 const { SimpleAdapter, ReverseProxyAdapter, WebHookListener } = require('twitch-webhooks');
 const { ApiClient, HelixStream } = require('twitch');
 const { ClientCredentialsAuthProvider } = require('twitch-auth');
-
+//config file
 const config = require('./config/config');
 
+
+//Logger
+const { format, loggers, transports } = require('winston')
+const { combine, timestamp, label, printf } = format;
+
+loggers.add('default', {
+    format: combine(
+        timestamp(),
+        printf(({ level, message, timestamp }) => {
+            return `${timestamp} ${level}: ${message}`;
+        })),
+    transports: [
+        new transports.Console({ 'timestamp': true, level: 'info' }),
+        (config.log_filename)?new transports.File({ 'timestamp': true, level: 'debug', filename: config.log_filename }):false,
+
+    ]
+})
+const logger = loggers.get('default');
+
+
+
+//Discord Bot Handler
 const DiscordHandler = require('./DiscordHandler')
 
+//Variable to limit number of name changes 
+var lastCheckedStatus = false;
 
 
 function checkConfig() {
     let requiredItems = ["hostname", "port", "channel_Id",
         "twitch_user_id", "offline_text", "online_text", "discord_bot_token", "twitch_clientId",
-        "twitch_accessToken"]
+        "twitch_accessToken", "cooldown_time"]
     for (let i = 0; i < requiredItems.length; i++) {
         let item = requiredItems[i];
         if (!config[item]) {
-            console.log(`Please Include '${item}' in the config.json file`)
+            logger.warn(`Please Include '${item}' in the config.json file`)
             return false;
         }
     }
@@ -27,10 +52,10 @@ async function start() {
     // console.log(checkConfig())
     if (!checkConfig()) {
 
-        console.log("Exiting");
+        logger.error("Config Check failed\nExisting")
         return;
     }
-
+    logger.info("Config Check Passed")
 
     const clientId = config.twitch_clientId
     const accessToken = config.twitch_accessToken;
@@ -38,7 +63,7 @@ async function start() {
     const apiClient = new ApiClient({ authProvider });
     const user = await apiClient.kraken.users.getUserByName(config.twitch_user_id)
     if (!user) {
-        console.log("User Not Found");
+        logger.error("User Not Found");
         return;
     }
 
@@ -50,27 +75,41 @@ async function start() {
     }));
 
 
-    const discordHandler = new DiscordHandler(config, async ()=>{
+    const discordHandler = new DiscordHandler(config, async () => {
         //Setting it the first Time 
         let twitchChannel = await apiClient.kraken.channels.getChannel(user);
         let stream = await twitchChannel.getStream();
-        discordHandler.changeName(stream);
+        lastCheckedStatus = !!stream;
+        handleStreamChange(stream);
     });
 
-    
-
-    
 
     const subscription = await listener.subscribeToStreamChanges(user._data._id, async (stream) => {
-        discordHandler.changeName(stream).then(() => {
-            console.log("Changing Name Done")
-        }).
-            catch((err) => {
-                console.log(err)
-            })
+        if (!!stream != lastCheckedStatus) {
+            handleStreamChange(stream)
+        } else {
+            logger.debug(`Stream Status Changed. Name will not be changed`)
+        }
+        lastCheckedStatus = !!stream;
 
     });
-    console.log('Subbed To Listener')
+    const handleStreamChange = async (stream) => {
+        var name = (!!stream ? config.online_text : config.offline_text).toString();
+
+        logger.info(`Stream Status Changed: ${name}`);
+        discordHandler.waitForTime(name).then(() => {
+            logger.info(`Changing Channel Name Done: ${name}`)
+        }).
+            catch((err) => {
+                if (err.canceled) {
+                    logger.debug(`Canceled: ${err.reason}`)
+                } else {
+                    logger.error(err)
+                }
+
+            })
+    }
+    logger.info('Subbed To Twitch Listener')
     listener.listen();
 }
 start()
